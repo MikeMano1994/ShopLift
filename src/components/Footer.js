cfe4fbbcf5dcdba4bb787f77718e94f6926d7062
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-04-08 02:44:15 -0700

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Footer extends Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+    render(){
+        return(
+            <footer class='footer'>
+                
+            </footer>
+        );
+    }
+}
