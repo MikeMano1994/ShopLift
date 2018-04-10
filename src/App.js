f1784e30c264221e13fe4ca531791f5f588a4064
Truc Vo <nhatruc3010@gmail.com>
2018-04-10 12:47:00 -0700

    add content component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './picture/logo.jpg';
 import Images from './components/Images';
-
+import Footer from './components/Footer';
+import Content from'./components/Content';
 import './App.css';
 import '../node_modules/react-image-slider/lib/image-slider.css';
 
@@ -13,8 +14,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">“Deals so great - It’s a steal”</h1>
         </div>
+        
         <div style={{marginTop:'200px'}}>
         <Images/>
+        </div>
+        <Content/>
+        <div className = "App-footer">
+        <Footer/>
         </div>
       </div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
+
 export default class Footer extends Component{
     constructor(props){
         super(props);
         this.state={};
     }
     render(){
+        const shareUrl = 'https://tuanandtruc.wixsite.com/shopl';
+        const title = 'ShopLift';
         return(
             <footer class='footer'>
-                
+
+                <p>© Copyright 2018 by ShopLift.</p>
+
             </footer>
         );
     }
