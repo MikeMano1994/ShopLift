c3ca20899dd02a0001e61571b286b00dff47ecf2
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-04-10 19:58:40 -0700

    carousel looks less weird

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,26 +16,28 @@ export default class Home extends Component{
     render(){
         return(
           <div>
-            <Carousel className='centered'>
+            <div className='carouselWrapper container' style={{width:'100',height:'500'}}>
+            <Carousel className='centered col-md-6 col-md-offset-3'>
             <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}} className='centered' width={900} height={500} alt="900x500" src={Zucchini}/>
+              <img className='centered' width={900} height={500} alt="900x500" src={Zucchini}/>
               <Carousel.Caption>
                 <h3>Zucchini</h3>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}}  width={900} height={500} alt="900x500" src={Pie}/>
+              <img width={900} height={500} alt="900x500" src={Pie}/>
               <Carousel.Caption>
                 <h3>Slow Baked Pie</h3>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}}  width={900} height={500} alt="900x500" src={Sandwich} />
+              <img width={900} height={500} alt="900x500" src={Sandwich} />
               <Carousel.Caption>
                 <h3>Sandwich</h3>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
+          </div>
           <br/>
           <Content/>
           </div>
