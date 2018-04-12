0f7247f8c7c610d86e32817f9951949640958560
Truc Vo <nhatruc3010@gmail.com>
2018-04-11 20:29:58 -0700

    fixed carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,7 @@ import React, {Component} from 'react';
 import {Carousel} from 'react-bootstrap';
 //import PopItems from './PopItems';
 import Content from './Content';
-
-import Zucchini from '../images/zucchini.jpg';
-import Pie from '../images/slow-cooker-pie.jpg';
-import Sandwich from '../images/sandwich.jpg';
+import Images from './Images';
 
 export default class Home extends Component{
     constructor(props){
@@ -16,34 +13,14 @@ export default class Home extends Component{
     render(){
         return(
           <div>
-            <div className='carouselWrapper container' style={{width:'100',height:'500'}}>
-              <Carousel className='centered col-md-6 col-md-offset-3'>
-                <Carousel.Item>
-                  <img className='centered' width={900} height={500} alt="900x500" src={Zucchini}/>
-                  <Carousel.Caption>
-                    <h3>Zucchini</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src={Pie}/>
-                  <Carousel.Caption>
-                    <h3>Slow Baked Pie</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src={Sandwich} />
-                  <Carousel.Caption>
-                    <h3>Sandwich</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>
-          </div>
-          <br/>
+       
+          <Images/>
           <br></br>
-          
+          <br></br>
           <Content/>
+
+         
+
           </div>
         );
     }
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,38 +1,57 @@
 import React, {Component} from 'react';
 import {Carousel} from 'react-bootstrap';
+//import PopItems from './PopItems';
 
-import Zucchini from '../images/zucchini.jpg';
-import Pie from '../images/slow-cooker-pie.jpg';
-import Sandwich from '../images/sandwich.jpg';
-
-export default class Images extends Component{
+import Bread from '../picture/slide/bread.jpg';
+import Pizza from '../picture/slide/pizza.jpg';
+import Steak from '../picture/slide/steak.jpg';
+import Pasta from '../picture/slide/pasta.jpg';
+export default class Home extends Component{
     constructor(props){
         super(props);
         this.state={};
     }
-    
-    render() {
+
+    render(){
         return(
-        <Carousel className='centered'>
-            <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}} className='centered' width={900} height={500} alt="900x500" src={Zucchini}/>
-              <Carousel.Caption>
-                <h3>Zucchini</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}}  width={900} height={500} alt="900x500" src={Pie}/>
-              <Carousel.Caption>
-                <h3>Slow Baked Pie</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img style={{paddingLeft:'37vw'}}  width={900} height={500} alt="900x500" src={Sandwich} />
-              <Carousel.Caption>
-                <h3>Sandwich</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+          <div>
+            <div className='carouselWrapper container' style={{width:'1000',height:'500'}}>
+              
+              <Carousel>
+                <Carousel.Item>
+                  <img className='centered' width={3000} height={1500} alt="1500x1500" src={Bread}/>
+                  <Carousel.Caption>
+                    <h3>Stuffed Bread Roll</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <img width={3000} height={1500} alt="900x500" src={Pizza}/>
+                  <Carousel.Caption>
+                    <h3>Pizza with Black Olive and Pepperoni</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <img width={3000} height={1500} alt="900x500" src={Steak} />
+                  <Carousel.Caption>
+                    <h3>Wagyu Steak</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                  <img width={3000} height={1500} alt="900x500" src={Pasta} />
+                  <Carousel.Caption>
+                    <h3>Italian Pasta</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              
+              </Carousel>
+
+           </div>
+      
+        </div>
+        
         );
     }
 }
