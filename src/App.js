61acbfeee697916e70b3e45e62c58a07c2a7b1d8
Tuan Anh Le <ale33@mail.ccsf.edu>
2018-04-13 01:52:46 -0700

    created Shop.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/layout/Header';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import AboutUs from  './components/AboutUs.js';
+import Shop from './components/Shop';
 
 import {BrowserRouter, Route} from 'react-router-dom';
 import {LinkContainer} from 'react-router-bootstrap';
@@ -28,6 +29,7 @@ class App extends Component {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/contact' component={Contact}/>
                 <Route exact path='/about-us' component={AboutUs}/>
+                <Route exact path='/shop' component={Shop}/>
               </div>
             </div>
           </BrowserRouter>
