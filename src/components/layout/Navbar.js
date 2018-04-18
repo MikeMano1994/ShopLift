be8d0772e934d034954770d1e416ac172c41bb51
Jennifer <july19.jenni@gmail.com>
2018-04-17 21:30:19 -0700

    edit search bar and added search icon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import DrawerCart from '../DrawerCart';
+import React, { Component } from "react";
+import DrawerCart from "../DrawerCart";
 
-import logo from '../../picture/logo.jpg';
-import user from '../../picture/user.png';
+import logo from "../../picture/logo.jpg";
+import user from "../../picture/user.png";
 //import shoppingcart from '../../picture/shopping-cart.png';
-import '../../App.css';
+import "../../App.css";
 
 export class Navbar extends Component {
   render() {
@@ -16,51 +16,66 @@ export class Navbar extends Component {
           </a>
         </div>
 
-        <div className= "bignavbar">
+        <div className="bignavbar">
           <nav className="navbar navbar-expand-lg navbar-white bg-white">
             <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <form className="form-inline my-0 my-sm-0">
+                  <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search for the fresh!"
+                    aria-label="Search"
+                  />
+                  <button
+                    className="btn btn-outline-success my-2 my-sm-0"
+                    type="submit"
+                  >
+                    <i class="glyphicon glyphicon-search" />
+                  </button>
+                </form>
+              </li>
 
-                <li className="nav-item">
-                  <form className="form-inline my-0 my-sm-0">
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search for the fresh!" aria-label="Search" />
-                  </form>
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/shop">
+                  Shop
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
+                  Contact
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/about-us">
+                  About Us
+                </a>
+              </li>
+
+              <ul className="nav navbar-right">
+                <li>
+                  <a href="/login">
+                    <img width={35} height={35} src={user} alt={user} />
+                    LOG IN
+                  </a>
                 </li>
-
-                <li className="nav-item active">
-                  <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                <li className="emptycart">
+                  <a>
+                    <DrawerCart />
+                  </a>
                 </li>
-
-                <li className="nav-item">
-                  <a className="nav-link" href="/shop">Shop</a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link" href="/contact">Contact</a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link" href="/about-us">About Us</a>
-                </li>
-
-                <ul className="nav navbar-right">
-                  <li>
-                    <a href="/login">
-                      <img width={35} height={35} src={user} alt={user}/>
-                      LOG IN
-                    </a>
-                  </li>
-                  <li className = "emptycart">
-                   <a>
-                      <DrawerCart/>
-                   </a>
-                  </li>
-                </ul>
-
+              </ul>
             </ul>
           </nav>
         </div>
-
       </div>
     );
   }
