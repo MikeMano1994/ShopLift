f13ec0e36ee04e8c1c768eec5d6b2c8eb3a35e70
Truc Vo <nhatruc3010@gmail.com>
2018-04-20 23:42:24 -0700

    hamburger style in navbar

--- src/components/layout/TrucNavBar.js
+++ src/components/layout/TrucNavBar.js
@@ -1,39 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import DrawerCart from "../DrawerCart";
 import UserDropDown from "../UserDropDown";
 
 import {
   Collapse,
   Navbar,
-  NavbarToggler,
   Nav,
   NavItem,
   NavLink,
   FormGroup,Form,
-  Input,
-  Button
-} from 'reactstrap';
+  Input, Button} from 'reactstrap';
 
 import logo from "../../picture/logo.jpg";
 import user from "../../picture/user.png";
 
+
 import "../../App.css";
 
-export default class NavBar extends Component {
-  constructor(props) {
-    super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+export default class TrucNavbar extends React.Component {
 
   render() {
     return (
@@ -43,28 +28,19 @@ export default class NavBar extends Component {
             <img className="App-logo" src={logo} alt="logo" />
           </a>
         </div>
-
-          <Navbar color="white" light expand="md">
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse isOpen={!this.state.collapsed} navbar>
-
-            <Nav navbar>
-              <NavItem>
-                <Form>
-                  <Input type="text" placeholder="Search for the fresh!" />
-                </Form>
-              </NavItem>
-              
-              <NavItem>
-              <Button type="submit" size="md" outline color="success" >
+          
+          <Navbar color="white" >
+            <Nav>
+              <Form>
+                <Input type="text" placeholder="Search for the fresh!" />
+              </Form> {' '}
+                
+              <Button type="submit" size="sm" outline color="success" >
                 <span class="glyphicon glyphicon-search my-sm-0"/>
               </Button>
-              </NavItem>
             </Nav>
 
-            
-
-
+          
             <Nav navbar >
               <NavItem>
                 <NavLink href="/">Home</NavLink>
@@ -90,15 +66,16 @@ export default class NavBar extends Component {
               <NavItem>
                 <UserDropDown />
               </NavItem>
-
+              
               <NavItem className="emptycart" >
                 <a><DrawerCart/></a>
               </NavItem>
             </Nav>
-
-             </Collapse>
         </Navbar>
       </div>
     );
   }
 }
+
+            
+     
