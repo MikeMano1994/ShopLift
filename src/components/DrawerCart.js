a31eeb902adb972bdd089d8a9aed727dfe76ca66
Bruce Lin <brucelin1218@gmail.com>
2018-04-20 18:05:58 -0700

    Refactor AboutUs.js by bootstrap. Add temparary teammember description

--- src/components/DrawerCart.js
+++ src/components/DrawerCart.js
@@ -4,18 +4,20 @@ import ReactDrawer from 'react-drawer';
 import shoppingcart from '../picture/shopping-cart.png';
 import emptycart from '../picture/empty-cart.png';
 
- 
+
 /* if you using webpack, should not apply identity to this css */
 import 'react-drawer/lib/react-drawer.css';
- 
+
 export default class DrawerCart extends Component{
   constructor() {
     super();
+
     this.state = {
       open: false,
       position: 'right',
       noOverlay: false
     };
+
     this.toggleDrawer = this.toggleDrawer.bind(this);
     this.closeDrawer = this.closeDrawer.bind(this);
     this.onDrawerClose = this.onDrawerClose.bind(this);
@@ -40,8 +42,8 @@ export default class DrawerCart extends Component{
     return (
       <div>
         <div>
-	     	<img width={35} height={35} src={shoppingcart} onClick={this.toggleDrawer}
-	            disabled={this.state.open }/>
+	     	   <img width={35} height={35} src={shoppingcart} onClick={this.toggleDrawer}
+	              disabled={this.state.open }/>
         </div>
 
         <ReactDrawer className="drawer"
@@ -50,20 +52,16 @@ export default class DrawerCart extends Component{
           onClose={this.onDrawerClose}
           noOverlay={this.state.noOverlay}
           >
-          
           <i onClick={this.closeDrawer} className="icono-cross"></i>
-          
-          <h2 className = "shoppingcartstyle">Shopping Cart</h2>
-	      <img style={{width:'250px', height:'150px'}} src={emptycart} alt="emptycart" />
-          <div>
-       		<a href="/check-out">
-          	<button type="button" class="btn btn-dark"><p>CHECK OUT</p></button></a>
-          </div>
 
-        
+          <h2 className = "shoppingcartstyle">Shopping Cart</h2>
+	        <img style={{width:'250px', height:'150px'}} src={emptycart} alt="emptycart" />
+          <div>
+       		   <a href="/check-out">
+          	 <button href="/check-out" type="button" class="btn btn-dark"><p>CHECK OUT</p></button></a>
+          </div>
         </ReactDrawer>
       </div>
     );
   }
 }
-
