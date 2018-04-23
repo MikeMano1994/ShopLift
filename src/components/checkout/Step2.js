028bcde1f78fd4d6d9890403a0e62ae125588a43
Tuan Anh Le <ale33@mail.ccsf.edu>
2018-04-23 15:44:16 -0700

    Step5, review info added

--- src/components/checkout/Step2.js
+++ src/components/checkout/Step2.js
@@ -6,7 +6,6 @@ import '../../App.css'
 export default class Step2 extends Component{
     constructor(props) {
     super(props);
-
     this.state = {};
     }
 
--- src/components/checkout/Step5.js
+++ src/components/checkout/Step5.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
+import {Button, Panel} from 'react-bootstrap';
 
 export default class Step5 extends Component{
 	 constructor(props) {
     super(props);
-
     this.state = {};
   	}
-  	
+
     render(){
         return(
-
-    	<h3> Review the information here</h3>
+        <div>
+    	<h1> Review and submit your order</h1>
+    	<div className = "shipping">
+    		<Panel>
+    		<Panel.Heading>Shipping Information</Panel.Heading>
+    		<Panel.Body>
+    			<p>Firstname:</p>
+      			<p>Lastname:</p>
+     			<p>Address:</p>
+      			<p>City:</p>
+      			<p>State:</p>
+      			<p>Zipcode:</p>
+    		</Panel.Body>
+    		</Panel>
+    	</div>
+    	<div className = "paymentInfo">
+    		<Panel>
+    		<Panel.Heading>Payment Information</Panel.Heading>
+    			<Panel.Body>
+    			<p>Cardname:</p>
+      			<p>Cardnumber:</p>
+     			<p>Expmonth:</p>
+      			<p>Expyear:</p>
+      			<p>cvv:</p>
+    			</Panel.Body>
+    		</Panel>
+    	</div>
+    	</div>
         );    
     }
 }
