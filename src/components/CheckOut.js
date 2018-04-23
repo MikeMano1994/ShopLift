15ca7cb7f2875e08b565293515bd4b65c16cd253
Tuan Anh Le <ale33@mail.ccsf.edu>
2018-04-23 00:31:23 -0700

    unused component

--- src/components/CheckOut.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class CheckOut extends Component{
-    render(){
-        return(
-		<h1> THIS IS EMPTY Check OUt </h1>
-        );    
-    }
-}
