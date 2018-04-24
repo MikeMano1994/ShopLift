5a9eaf73806222062790d2432530f01b2f4d9b86
Truc Vo <nhatruc3010@gmail.com>
2018-04-24 11:05:46 -0700

    move search bar to shop, and fixed the navbar

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import Products from "./Products";
 import Sidebar from './Sidebar';
+import {Form, FormGroup,
+        Input,
+        Button
+} from 'reactstrap';
+import product from './product.json';
+
+import fire from '../fire';
 
 export default class Shop extends Component{
 	constructor(props){
     super(props);
     this.state={
-      category: 'all'
+      category: 'all',
+      products: {},
+      unfilteredProducts:{}
+
     };
     this.setCategory = this.setCategory.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+
   }
 
   setCategory(c){
@@ -16,17 +28,45 @@ export default class Shop extends Component{
       this.setState({category: c});
     }
   }
+  handleSearch(event){
+    let filteredProducts = this.state.unfilteredProducts;
+    filteredProducts = filteredProducts.filter(i => i.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1 || i.category.toLowerCase().search(event.target.value.toLowerCase()) !== -1)
+    this.setState({products: filteredProducts});
+  }
+   componentWillMount(){
+    this.setState({products: product});
+    this.setState({unfilteredProducts: product});
+  }
+
 
   render(){
       return(
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xs-12'>
-              <Sidebar setCategory={this.setCategory}/>
-            </div>
-            <br/>
-            <div className='col-xs-12'>
-              <Products addToCart={this.props.addToCart} category={this.state.category}  productsList={this.props.productsList}/>
+        <div >     
+          <div className='container'>
+            <div className='row'>
+            
+              <div className='col-xs-12'>
+                <Sidebar setCategory={this.setCategory}/>
+              </div>
+              
+              <FormGroup>
+              <Form className="movetotheright" >
+                <Input className = "searchbar"
+                  type="text" placeholder="Search for the fresh!" onChange = {this.handleSearch}/>
+              </Form>     
+              </FormGroup>
+
+              <FormGroup>
+              <Button type="submit" size="lg" outline color="success" >
+                <span className="glyphicon glyphicon-search my-sm-0"/>
+              </Button>
+              </FormGroup>
+
+
+              <br/>
+              <div className='col-xs-12'>
+                <Products addToCart={this.props.addToCart} category={this.state.category} productsList={this.state.products}/>
+              </div>
             </div>
           </div>
         </div>
