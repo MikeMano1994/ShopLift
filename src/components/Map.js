083dfb7a25efb5201b06a942562562b42fa17fa0
Bruce Lin <brucelin1218@gmail.com>
2018-04-25 00:38:16 -0700

    Fix the Google Maps overlayed under the footer

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,4 @@
-
 import React, {Component} from 'react';
-
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
@@ -27,37 +25,29 @@ export class MapContainer extends Component {
   };
 
   render() {
-    const style = {
-      width: '500px',
-      height: '380px'
-  }
+    return (
+      <div style={{width:'500px', height:'500px'}}>
+        <Map
+          google={this.props.google}
+          initialCenter={{ lat: 37.3357807, lng: -121.8821639 }}
+          zoom={15}
+          onClick={this.onMapClicked}
+          responsive
+        >
+          <Marker onClick={this.onMarkerClick}
+            name={'ShopLift'} />
 
-  return (
-    <div style={{width : '500' , height : '500'}} >
-      <Map
-        google={this.props.google}
-        style={style}
-        initialCenter={{
-          lat: 37.3357807,
-          lng: -121.8821639
-        }}
-        zoom={11}
-        onClick={this.onMapClicked}
-      >
-        <Marker onClick={this.onMarkerClick}
-          name={'We live here'} />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
 
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-
-        <div>
-          <h1>{this.state.selectedPlace.name}</h1>
-        </div>
-        </InfoWindow>
-      </Map>
-    </div>
-    );
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+          </InfoWindow>
+        </Map>
+      </div>
+      );
   }
 }
 
